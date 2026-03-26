85b7484a038b2394fd097706a0d13b8f2baaa5da
Ethan Li <yli2494@wisc.edu>
2026-03-26 17:41:14 -0500

    working on fsm, might pivot from ISA idea to memory mapped io

--- linear_ramp.sv
+++ linear_ramp.sv
@@ -1,3 +1,22 @@
+/*[Implementation Notes]
+Start with reading in the enable signal, generate a one cycle pulse that represents rising edge of enable signal. This will be enable_pulse
+
+enable_pulse high = first cycle the module is active. We will need to calculate the increment amount at this clock cycle with the divide module
+
+==================== divide module ======================
+ONLY ACTIVE IF enable_pulse is 1. 
+When active, input flops go transparent to load in values to calculate. 
+
+calculate the value (v_to - v_from) / duration , store into a global register within module (inc_amt), then increment v_drive by inc_amt.
+
+On falling edge of enable_pulse, make input flops opaque to save power
+
+==================== accumulator module ======================
+ALWAYS ACTIVE WHILE MODULE IS ACTIVE.
+At each clock cycle, increment v_drive by inc_amt, until module deasserts
+*/
+
+
 module delay_gen (
     input clk,
     input rst_n,
@@ -8,7 +27,7 @@ module delay_gen (
 
     output [13:0] v_drive
 );
-    //register for determining en_pulse
+    //register for determining en_pulse from en signals
     wire en_ff, en_pulse;
     always_ff @ (posedge clk, negedge rst_n) begin
         if (!rst_n) en_ff <= '0;
@@ -16,7 +35,7 @@ module delay_gen (
     end
     assign en_pulse = en && !en_ff;
 
-    //comb block that calculates the v diff
+    //comb block that calculates v_diff
     wire signed [13:0] v_diff
     always_comb begin
         v_diff = $signed(v_to) - $signed(v_from);
